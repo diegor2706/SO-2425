90a2e864fa4a8c7232f82dcb1d826f44d88c3089
Diego Román Pose <124998332+diegor2706@users.noreply.github.com>
2024-10-08 13:49:11 +0200

    Práctica 1

---
+++ p1/Lista.c
@@ -0,0 +1,69 @@
+// para el historial
+#include "Lista.h"
+#include <stdlib.h>
+#include <string.h>
+
+void createEmptyList(tList *L){
+    *L = LNULL;
+}
+bool isEmptyList(tList L){
+    return (L == LNULL);
+}
+bool createNode(tPosL *p){
+    *p = malloc(sizeof(struct tNode));
+    return *p != LNULL;
+}
+
+bool insertItem(tItemL d, tPosL p, tList *L) {
+    /* q -> elemento que se va a insertar
+     * r -> elemento previo a q */
+    tPosL q, r;
+
+    if(!createNode(&q)) {
+        return false;
+    } else {
+        strcpy(q->elemento.datos, d);
+        q->siguiente = LNULL;
+        q->anterior = LNULL;
+        if(isEmptyList(*L)) {
+            *L = q;
+        } else if(p == LNULL) {
+            for(r = *L; r->siguiente != LNULL; r = r->siguiente);
+            r->siguiente = q;
+            q->anterior = r;
+
+        } else if(p == *L) {
+            q->siguiente = p;
+            p->anterior = q;
+            *L = q;
+
+        } else {
+
+            strcpy(q->elemento.datos, p->elemento.datos);
+            strcpy(p->elemento.datos , d);
+            if(p->siguiente != LNULL) {
+                p->siguiente->siguiente = q;
+            }
+            q->siguiente = p->siguiente;
+            p->siguiente = q;
+            q->anterior = p;
+        }
+        return true;
+    }
+
+}
+
+tPosL first(tList L){
+    return L;
+}
+
+tPosL last(tList L){
+    tPosL p;
+
+    for(p = L; p->siguiente != LNULL; p = p->siguiente);
+    return p;
+}
+
+tPosL next(tPosL p, tList L){
+    return p->siguiente;
+}
---
+++ p1/Lista.h
@@ -0,0 +1,33 @@
+// para el historial
+
+#ifndef p1_LISTA_H
+#define p1_LISTA_H
+#include <stdbool.h>
+
+#define LNULL NULL
+typedef char tItemL[30];
+
+typedef struct element{
+    tItemL datos;
+}element;
+
+typedef struct tNode *tPosL;
+
+struct tNode{
+    element elemento;
+    tPosL siguiente;
+    tPosL anterior;
+};
+
+typedef tPosL tList;
+
+void createEmptyList(tList *L);
+bool isEmptyList(tList L);
+bool createNode(tPosL *p);
+bool insertItem(tItemL d, tPosL p, tList *L);
+tPosL first(tList L);
+tPosL last(tList L);
+tPosL next(tPosL p, tList L);
+
+
+#endif //p1_LISTA_H
