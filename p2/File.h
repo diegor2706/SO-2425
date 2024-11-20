6599f72cac651295d7ba60d9e45660d51056486f
Diego Román Pose <124998332+diegor2706@users.noreply.github.com>
2024-11-20 12:44:16 +0100

    Update File.h

--- p2/File.h
+++ p2/File.h
@@ -1,42 +1,124 @@
 // para el open
-
-#ifndef p2_FILE_H
-#define p2_FILE_H
-#define FNULL NULL
-
-#include <stdbool.h>
-
-#define LNULL NULL
-#define  MAX 30
-
-typedef int Descriptor;
-
-typedef struct elementF{
-    Descriptor descriptor;
-    char nombre[MAX];
-    char modo[MAX];
-    int offset;
-}elementF;
-
-typedef struct tNodeF *tPosF;
-
-struct tNodeF {
-    elementF data;
-    tPosF siguiente;
-};
-
-typedef tPosF tListF;
-
-void createEmptyFileList(tListF *F);
-bool isEmptyFileList(tListF F);
-bool createFileNode(tPosF *p);
-bool insertFileItem(Descriptor j, char *m,char *n, int offset,  tPosF p, tListF *L);
-tPosF firstFile(tListF L);
-tPosF lastFile(tListF L);
-tPosF nextFile(tPosF p, tListF L);
-tPosF findFileItem(int x, tListF L);
-void deleteFileAtPosition(tPosF p, tListF *L);
-void deleteFileList(tListF *F);
+#include "File.h"
+#include <stdlib.h>
+#include <string.h>
 
 
-#endif //p2_FILE_H
+void createEmptyFileList(tListF *F) {
+    *F = LNULL;
+}
+
+bool isEmptyFileList(tListF F) {
+    return (F == LNULL);
+}
+
+bool createFileNode(tPosF *p) {
+
+    *p = malloc(100 * sizeof(struct tNodeF));
+    return *p != LNULL;
+}
+
+bool insertFileItem(Descriptor j, char  *m,char  *n,int offset, tPosF p, tListF *L) {
+
+    tPosF q, r;
+
+    if(!createFileNode(&q)) {
+        return false;
+    }
+    else {
+        strcpy(q->data.nombre, n);
+        strcpy(q->data.modo, m);
+        q->data.descriptor = j;
+        q->data.offset = offset;
+
+        q->siguiente = LNULL;
+
+        if (isEmptyFileList(*L)) {
+            *L = q;
+        }
+
+        else if(p == LNULL) {
+            for(r = *L; r->siguiente != LNULL; r = r->siguiente);
+
+            r->siguiente = q;
+        }
+
+        else if(p == *L) {
+            q->siguiente = p;
+            *L = q;
+        }
+
+        else {
+            q->data = p->data;
+
+            strcpy(p->data.nombre, n);
+            strcpy(p->data.modo, m);
+            p->data.descriptor = j;
+            p->data.offset = offset;
+
+
+            q->siguiente = p->siguiente;
+            p->siguiente = q;
+        }
+        return true;
+    }
+}
+
+tPosF nextFile(tPosF p, tListF F) {
+    return p->siguiente;
+}
+
+tPosF firstFile(tListF L) {
+    return L;
+}
+
+tPosF lastFile(tListF F){
+    tPosF i;
+    for (i = F; i->siguiente != FNULL ; i = i->siguiente);
+    return i;
+}
+
+tPosF findFileItem(int x, tListF F) {
+    tPosF i = F;
+
+    while (i != FNULL) {
+        if (i->data.descriptor == x) {
+            break;
+        }
+        i = i->siguiente;
+    }
+
+    return i;
+}
+
+
+void deleteFileAtPosition(tPosF p, tListF *F) {
+
+    tPosF q;
+
+    if(p == *F) { // Eliminado en la primera posición de la lista
+        *F = (*F)->siguiente;
+
+    } else if(p->siguiente == FNULL) { // Eliminado en la última posición
+        for(q = *F; q->siguiente->siguiente != LNULL; q = q->siguiente);
+        q->siguiente = LNULL;
+
+    } else { // Eliminado de un elemento que no se encuentra en los extremos
+        q = p->siguiente;
+        p->data = q->data;
+        p->siguiente = q->siguiente;
+        p = q;
+    }
+
+    free(p);
+}
+
+void deleteFileList(tListF *F) {
+    tPosF p;
+
+    while(!isEmptyFileList(*F)) {
+        p = *F;
+        *F = (*F)->siguiente;
+        free(p);
+    }
+}
